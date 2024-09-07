12536043d3363d2054e26bbd16407ad9b09d9455
Pumagon <81986311+Pumagon@users.noreply.github.com>
2024-09-06 19:53:07 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# [wifi]
+ssid = "Not Connected"
+password = "r3avnpmmfr!234"
+wifiRetrySec = 1
+
+# [time]
+ntpserver = "us.pool.ntp.org"
+timezone_offset_pdt = -7 * 60 * 60 # PDT
+timezone_offset_pst = -8 * 60 * 60 # PST
+ntpRetrySec = 1
+
+# [display]
+thisMachineHasDisplay = True
+thisMachineHasConsole = False
+
+# [kintone]
+sdomain = "karube"
+appId = "19"
+token = "ZBEVk7MQ7W1YFQCTXKowOEPqCyWvqZOJNgUFMPtd"
+
+# [bmi160]
+activityThreshold = 0.1
+i2cChannel = 0
+sda = 16
+scl = 17
